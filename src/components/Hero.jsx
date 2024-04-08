a94eabd17746fffdbf09dca0aa186f1b65e46bad
aOzSparKz <joepaulsajot@gmail.com>
2024-04-08 20:02:41 +0800

    change tamsak

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,14 +14,14 @@ const Hero = () => {
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
-        <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          IT Department
-          </p>  
+     
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className='text-[#915EFF]'>Paul</span>
           </h1>
-       
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          IT Department
+          </p>  
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
           For technical support<br className='sm:block hidden' />
           </p>  
